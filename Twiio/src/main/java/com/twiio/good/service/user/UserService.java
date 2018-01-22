d2728587034fa8e7b9cfd18c8eab874ec04e0110
장은애 <@DESKTOP-TPCD2TL>
2018-01-22 16:40:51 +0900

    2018-01-22-16:40 :USER SERVICE, DAO 수정

--- Twiio/src/main/java/com/twiio/good/service/user/UserService.java
+++ Twiio/src/main/java/com/twiio/good/service/user/UserService.java
@@ -1,5 +1,41 @@
 package com.twiio.good.service.user;
 
+import java.util.List;
+import java.util.Map;
+
+import com.twiio.good.common.Search;
+import com.twiio.good.service.domain.User;
+
+
 public interface UserService {
+	
+	// 회원가입
+	public void addUser(User user) throws Exception;
+	
+	// 내정보확인 / 로그인
+	public User getUser(String userId) throws Exception;
+	
+	// 회원정보리스트 
+	public Map<String , Object> listUser(Search search) throws Exception;
+	
+	// 회원정보수정
+	public void updateUser(User user) throws Exception;
+	
+	public void delectUser(User user) throws Exception;
+	
+	public void addEvalUser(User tagetUser, User evalUser, String scheduleNo) throws Exception;
+	
+	public List getStarEvalHost(User user) throws Exception;
+	
+	public Map listBestHost(User user) throws Exception;
+	
+	//public void addStarEvalHost(Transaction transaction) throws Exception;
+	
+	public String  getEvalUser(String userId) throws Exception;
+	
+	public String  getEvalHost(String userId) throws Exception;
+	
+	// 회원 ID 중복 확인
+	public boolean checkDuplication(String userId) throws Exception;
 
 }
--- Twiio/src/main/java/com/twiio/good/service/user/impl/UserServiceImpl.java
+++ Twiio/src/main/java/com/twiio/good/service/user/impl/UserServiceImpl.java
@@ -1,9 +1,93 @@
 package com.twiio.good.service.user.impl;
 
-public class UserServiceImpl {
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+import com.twiio.good.common.Search;
+import com.twiio.good.service.domain.User;
+import com.twiio.good.service.user.UserDao;
+import com.twiio.good.service.user.UserService;
+
+
+//==> 회원관리 서비스 구현
+@Service("userServiceImpl")
+public class UserServiceImpl implements UserService{
+	
+	///Field
+	@Autowired
+	@Qualifier("userDaoImpl")
+	private UserDao userDao;
+	public void setUserDao(UserDao userDao) {
+		this.userDao = userDao;
+	}
+	
 
 	public UserServiceImpl() {
-		// TODO Auto-generated constructor stub
+		System.out.println(this.getClass());
+	}
+
+	@Override
+	public void addUser(User user) throws Exception {
+		userDao.addUser(user);
+	}
+
+	@Override
+	public User getUser(String userId) throws Exception {
+		// TODO Auto-generated method stub
+		return userDao.getUser(userId);
+	}
+
+	@Override
+	public Map<String, Object> listUser(Search search) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void updateUser(User user) throws Exception {
+		// TODO Auto-generated method stub
+		userDao.updateUser(user);
+	}
+
+	@Override
+	public void delectUser(User user) throws Exception {
+		// TODO Auto-generated method stub
+		userDao.delectUser(user);
+	}
+
+	@Override
+	public void addEvalUser(User tagetUser, User evalUser, String scheduleNo) throws Exception {
+		userDao.addEvalUser(tagetUser, evalUser, scheduleNo);
+	}
+
+	@Override
+	public List getStarEvalHost(User user) throws Exception {
+		return userDao.getStarEvalHost(user);
+	}
+
+	@Override
+	public Map listBestHost(User user) throws Exception {
+		return null;
+	}
+
+	@Override
+	public String getEvalUser(String userId) throws Exception {
+		return userDao.getEvalUser(userId);
+	}
+
+	@Override
+	public String getEvalHost(String userId) throws Exception {
+		return userDao.getEvalHost(userId);
+	}
+
+	@Override
+	public boolean checkDuplication(String userId) throws Exception {
+		// TODO Auto-generated method stub
+		return false;
 	}
 
 }
