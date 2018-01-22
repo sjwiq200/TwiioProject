1847ae9705f75b681550677d1b74326ed270664d
bitcamp <bitcamp@bit>
2018-01-22 20:41:53 +0900

    dayoung : dailyPlan Service 작성중

--- Twiio/src/main/java/com/twiio/good/service/dailyplan/DailyPlanDao.java
+++ Twiio/src/main/java/com/twiio/good/service/dailyplan/DailyPlanDao.java
@@ -1,5 +1,18 @@
 package com.twiio.good.service.dailyplan;
 
+import java.util.List;
+
+import com.twiio.good.service.domain.DailyPlan;
+
 public interface DailyPlanDao {
 
+	public void addDailyPlan(DailyPlan dailyPlan);
+	
+	public DailyPlan getDailyPlan(int dailyPlanNo);
+	
+	public void updateDailyPlan(DailyPlan dailyPlan);
+
+	public List<DailyPlan> getDailyPlanList(DailyPlan dailyPlan);
+
+	public void DailyPlan(int dailyPlanNo);
 }
--- Twiio/src/main/java/com/twiio/good/service/dailyplan/impl/DailyPlanDaoImpl.java
+++ Twiio/src/main/java/com/twiio/good/service/dailyplan/impl/DailyPlanDaoImpl.java
@@ -1,9 +1,61 @@
 package com.twiio.good.service.dailyplan.impl;
 
-public class DailyPlanDaoImpl {
+import java.util.List;
 
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Repository;
+
+import com.twiio.good.service.dailyplan.DailyPlanDao;
+
+//@Repository("dailyPlanDaoImpl")
+public class DailyPlanDaoImpl implements DailyPlanDao{
+	
+	///Field
+		@Autowired
+		@Qualifier("sqlSessionTemplate")
+		private SqlSession sqlSession;
+		
+		
 	public DailyPlanDaoImpl() {
-		// TODO Auto-generated constructor stub
 	}
+
+
+	@Override
+	public void addDailyPlan(DailyPlan dailyPlan) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public DailyPlan getDailyPlan(int dailyPlanNo) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	@Override
+	public void updateDailyPlan(DailyPlan dailyPlan) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public List<DailyPlan> getDailyPlanList(DailyPlan dailyPlan) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	@Override
+	public void DailyPlan(int dailyPlanNo) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
 
 }
