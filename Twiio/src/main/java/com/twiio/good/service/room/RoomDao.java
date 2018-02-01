d3e1dc76db5e6d43ab2894ee4836487f799f1888
JW <sjwiq200@naver.com>
2018-02-01 21:01:48 +0900

    Update RoomController & RoomRestController new/jaewoo 2018.02.01

--- Twiio/src/main/java/com/twiio/good/service/room/RoomDao.java
+++ Twiio/src/main/java/com/twiio/good/service/room/RoomDao.java
@@ -4,13 +4,20 @@ import java.util.List;
 
 import com.twiio.good.common.Search;
 import com.twiio.good.service.domain.Room;
+import com.twiio.good.service.domain.RoomUser;
 import com.twiio.good.service.domain.User;
 
 
 public interface RoomDao {
 	
-public List<Room> getRoomList(Search search) throws Exception;
+	public List<Room> listRoom(Search search) throws Exception;
 	
 	public void addRoom(Room room) throws Exception;
+	
+	public void addRoomUser(String roomKey, int userNo) throws Exception;
+	
+	public List<RoomUser> listMyRoom(int userNo) throws Exception;
+	
+	public Room getRoom(String roomKey) throws Exception;
 
 }
