eb7018a4d8050dc9dbac9adedc7c3a8fcb50c273
bitcamp <bitcamp@bit-109>
2018-02-15 17:53:45 +0900

    daeun: mainplan Rest

--- Twiio/src/main/java/com/twiio/good/web/mainplan/MainPlanRestController.java
+++ Twiio/src/main/java/com/twiio/good/web/mainplan/MainPlanRestController.java
@@ -1,7 +1,11 @@
 package com.twiio.good.web.mainplan;
 
+import java.io.File;
 import java.net.URLDecoder;
+import java.sql.Date;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
@@ -62,7 +66,7 @@ public class MainPlanRestController {
 	}
 	
 	@RequestMapping(value = "json/listMainPlan", method = RequestMethod.POST)
-	public List<MainPlan> selectCity(	@RequestBody User user) throws Exception {
+	public List<MainPlan> listMainPlan(	@RequestBody User user) throws Exception {
 		
 		System.out.println("RestController : listMainPlan <START>");
 		System.out.println("user :: "+user);
@@ -82,7 +86,96 @@ public class MainPlanRestController {
 		
 	}
 	
-	
+	@RequestMapping(value = "json/addMainPlan", method = RequestMethod.POST)
+	public Boolean addMainPlan(	@RequestBody MainPlan mainPlan) throws Exception {
+		
+		System.out.println("RestController : addMainPlan <START>");
+		System.out.println("mainPlan :: "+mainPlan);
+		
+		String countryResult="";
+		
+		for(int i = 0 ; i < mainPlan.getCountryList().length;i++) {
+			if(i== mainPlan.getCountryList().length-1) {
+				countryResult +=  mainPlan.getCountryList()[i];
+			}else {
+				countryResult +=  mainPlan.getCountryList()[i] + ",";
+			}
+		}
+		
+		User user = userService.getUser(mainPlan.getUser().getUserId());
+		//mainPlan.setCity(cityResult);
+		mainPlan.setUser(user);
+		mainPlan.setCountry(countryResult);
+		mainPlan.setCity("no");
+//		System.out.println("getOriginalFilename :: "+mainPlan.getFile().getOriginalFilename());
+//		String fileName = user.getUserNo()+"="+ mainPlan.getFile().getOriginalFilename();
+//		System.out.println("fileName :: "+fileName);
+//		mainPlan.setMainThumbnail(fileName);
+//		System.out.println("######debug : " + mainPlan);
+//		File file = new File(mainPlanFilePath, fileName);
+//		mainPlan.getFile().transferTo(file);
+//		
+		System.out.println("addMainPlan 직전 :::::");
+		mainPlanService.addMainPlan(mainPlan);
+
+
+	      System.out.println("플랜제목 : " + mainPlan.getPlanTitle());
+	      System.out.println("출발일 : "+ mainPlan.getDepartureDate());
+	      System.out.println("도착일 : " + mainPlan.getArrivalDate());
+	      //System.out.println("여행지 : " + mainPlan.getCity());
+	      
+	      DailyPlan dailyPlan = new DailyPlan();
+	      dailyPlan.setMainPlan(mainPlan);
+	      dailyPlan.setUser(user);
+	      
+	      Date departureDate = mainPlan.getDepartureDate();
+	      Date arrivalDate = mainPlan.getArrivalDate();
+	      
+	      if(departureDate != arrivalDate) {
+	         java.util.Date departureDateUtil = departureDate;
+	         java.util.Date arrivalDateUtil = arrivalDate;
+	         
+	         long diff = arrivalDateUtil.getTime() - departureDateUtil.getTime();
+	           long diffDays = diff / (24 * 60 * 60 * 1000);
+	           
+	           Date dailyDate = mainPlan.getDepartureDate();
+	           
+	           for(int i = 0 ; i <diffDays+1 ; i++) {
+	              
+	              //dailyPlan.setDailyCity(cityResult);
+	             //dailyPlan.setDailyCountry(mainPlan.getCountry());
+	             dailyPlan.setDay(i+1);
+	             dailyPlan.setDailyDate(dailyDate);
+	             dailyPlan.setMainPlan(mainPlan);
+	             dailyPlan.setUser(user);
+	             
+	             dailyPlanService.addDailyPlan(dailyPlan); 
+	             
+	              Calendar calendar = Calendar.getInstance();//+1위해 Calendar형식으로 변환
+	              calendar.setTime(dailyDate);
+	              calendar.add(Calendar.DATE,1);
+	              java.util.Date calDailyDate = calendar.getTime();//Fri Jan 05 00:00:00 KST 2018
+
+	      
+	              final String FORMAT = "yyyy-MM-dd";
+	              SimpleDateFormat simpleDateFormatString = new SimpleDateFormat(FORMAT);
+	              String dailyDateFormat = simpleDateFormatString.format(calDailyDate);//String값
+	              dailyDate=Date.valueOf(dailyDateFormat);
+	           }
+	      } else {
+	         //dailyPlan.setDailyCity(cityResult);
+	           //dailyPlan.setDailyCountry(mainPlan.getCountry());
+	           dailyPlan.setDay(1);
+	           dailyPlan.setDailyDate(mainPlan.getArrivalDate());
+	           dailyPlan.setMainPlan(mainPlan);
+	           dailyPlan.setUser(user);
+	      }
+		
+	    System.out.println("RestController : addMainPlan <END>");  
+	      
+		return true;
+		
+	}
 	
 	
 }
